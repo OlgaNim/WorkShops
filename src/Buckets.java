34200ef4a0eebb76061ed6b8f34aaa373902601c
Olga Nim <helganim@list.ru>
2024-07-16 13:09:51 +0300

    add solution

--- src/Buckets.java
+++ src/Buckets.java
@@ -1,11 +1,25 @@
 public class Buckets {
-    public boolean isValid(String s) {
+    public static boolean isValid(String s) {
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        boolean answ = true;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {count1 += count1;}
+            else if (s.charAt(i) == ')') {count1 -= count1;}
+            else if (s.charAt(i) == '[') {count2 += count2;}
+            else if (s.charAt(i) == ']') {count2 -= count2;}
+            else if (s.charAt(i) == '{') {count3 += count3;}
+            else if (s.charAt(i) == '}') {count3 -= count3;}
+        }
+        if (count1  == 0 && count2 == 0 && count3 == 0) {
+            return true;
+        }
+        return false;
 
-        return (s.indexOf('(') < s.indexOf(')') && (s.indexOf('(') >= 0 && s.indexOf(')') >= 0)
-                || s.indexOf('[') < s.indexOf(']') && (s.indexOf('[') >= 0 && s.indexOf(']') >= 0)
-                || s.indexOf('{') > s.indexOf('}')) && (s.indexOf('{') >= 0 && s.indexOf('}') >= 0);
     }
     public static void main(String[] args) {
+        isValid("(]");
     }
 }
 
