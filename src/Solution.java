c892b0ae498f4dec100c36a36589db8834817dd5
Olga Nim <helganim@list.ru>
2024-07-16 21:01:29 +0300

    add solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,22 @@
 class Solution {
     public static void reverseString(char[] s) {
-        int indexOfChar = 0;
-        int arraysLength = s.length;
-        char[] d = new char[arraysLength];
-        for (int i = arraysLength - 1; i >= 0; i--) {
-            d[indexOfChar] = s[i];
-            indexOfChar += 1;
+        int n = s.length;
+       //char[] d = new char[s.length];
+
+        for (int i = 0; i < n; i++) {
+
+            var temp = s[n -i -1];
+            s[n -i -1] = s[i];
+            s[i] = temp;
+
+
         }
-        s = d;
+
         System.out.println(s);
     }
 
-    public static void test() {
+    public static void main(String[] args) {
         reverseString(new char[]{'h', 'e', 'l', 'l', 'o'});
     }
-    public static void main(String[] args) {
-        test();
-    }
 }
-
 
